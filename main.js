f2c6af5ce2e42ce7793f37f1b95b6647c9532b15
LegoTed2000 <112373501+LegoTed2000@users.noreply.github.com>
2023-12-07 17:05:58 +1100

    Add files via upload

--- main.js
+++ main.js
@@ -1,20 +1,66 @@
+objects = [];
+Status = "";
+
 function preload() {
 
 }
 
 function setup() {
-    canvas = createCanvas(800, 700);
+    canvas = createCanvas(400, 400);
     canvas.center();
     video = createCapture(VIDEO);
+    video.size();
     video.hide();
 }
 
 function draw() {
-    image(video, 0, 0, 800, 700);
+    image(video, 0, 0, 400, 400);
+
+    if(Status != "") {
+
+        objectDetector.detect(video, gotResults);
+
+        for(i = 0; i < objects.length; i++){
+            document.getElementById("status").innerHTML = "Objects Detecter";
+
+            r = Math.random(255);
+            g = Math.random(255);
+            b = Math.random(255);
+
+            fill(r, g, b);
+            percent = floor(objects[i].confidence * 100)
+            noFill();
+            text(objects[i].label + " " + percent + "%", objects[i].x + 15, objects[i].y + 15);
+            stroke(r, g, b);
+            rect(objects[i].x , objects[i].y, objects[i].width, objects[i].height);
+
+            if(objects[i].label == input) {
+                video.stop();
+                objectDetector.detect(gotResults);
+                document.getElementById("status").innerHTML = "Found"
+            }
+            else {
+                document.getElementById("status").innerHTML = "Status : Not Found";
+            }
+        }
+    }
 }
 
 function start() {
+    objectDetector = ml5.objectDetector("cocossd", modelLoaded);
+    document.getElementById("status").innerHTML = "Status : Objects Detected";
+    input = document.getElementById("thing").value;
+}
 
-    document.getElementById("status").innerHTML = "Status : Objects Detected"
-    input = document.getElementById("input").value;
+function gotResults(error , results) {
+    if(error) {
+        console.log(error);
+    }
+    console.log(results);
+    objects = results;
+}
+
+function modelLoaded() {
+    console.log( "It worked");
+    Status = true;
 }
